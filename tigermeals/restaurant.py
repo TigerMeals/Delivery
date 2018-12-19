abdddb85d9eec440daf80e962aee8d435ad4adf8
Janet Lee <janetlee2016@gmail.com>
2018-12-19 13:29:44 -0800

    silly janet you forgot to change the db url

--- tigermeals/restaurant.py
+++ tigermeals/restaurant.py
@@ -29,8 +29,8 @@ cloudinary.config(
 	api_secret = "gQUTMItrljBmcZ2Po8cbVvEFJvU"
 )
 
-# DATABASE_URL = "http://hidden-springs-97786.herokuapp.com"
-DATABASE_URL="http://localhost:5000"
+DATABASE_URL = "http://hidden-springs-97786.herokuapp.com"
+#DATABASE_URL="http://localhost:5000"
 
 # Endpoint to logout a restaurant
 @app.route("/restaurant/logout")
