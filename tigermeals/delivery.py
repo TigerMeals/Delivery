2b2ee2f1c0c81377ed250a997084053a7f757929
Janet Lee <janetlee2016@gmail.com>
2019-01-03 01:47:03 -0800

    deploy to heroku

--- tigermeals/delivery.py
+++ tigermeals/delivery.py
@@ -16,8 +16,8 @@ import cloudinary.utils
 cas = CAS(app, '/cas')
 cas.init_app(app)
 
-# DATABASE_URL = "http://tigermeals.herokuapp.com"
-DATABASE_URL="http://localhost:5000"
+DATABASE_URL = "http://hidden-springs-97786.herokuapp.com"
+# DATABASE_URL="http://localhost:5000"
 
 app.secret_key = 'dfasdkfjadkjfasdkjfhasdkjfh'
 app.config['CAS_SERVER'] = 'https://fed.princeton.edu'
